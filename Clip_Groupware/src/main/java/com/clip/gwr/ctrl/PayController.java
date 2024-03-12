6653119acc540ab414d17ca13763cb4d30f9c5f0
Kim Dong Hui <ehdgnl971000@kakao.com>
2024-03-12 15:15:56 +0900

    2024.03.12 header.jsp 마무리작업 empty.jsp 수정

--- Clip_Groupware/src/main/java/com/clip/gwr/ctrl/PayController.java
+++ Clip_Groupware/src/main/java/com/clip/gwr/ctrl/PayController.java
@@ -3,12 +3,12 @@ package com.clip.gwr.ctrl;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.clip.gwr.model.service.IGianService;
 import com.clip.gwr.vo.GianVo;
@@ -48,9 +48,17 @@ public class PayController {
 	
 	@GetMapping(value="/gianModify.do")
 	public String gianModify(Model model,HttpServletRequest request) {		
-		log.info("PayController 기안서 수정 페이지 ");
-		String seq=request.getParameter("gian_seq");
-		
+		log.info("PayController gianModify 수정 GET");
+		String seq = request.getParameter("gian_seq");
+		GianVo vo = service.templateDetail(seq);
+		model.addAttribute("vo",vo);
 		return "gianModify";
+	}
+	
+	@PostMapping(value="/gianMod.do")
+	public String gianModify(String gian_html) {
+		log.info("PayController gianModify 수정 POST :{}",gian_html);
+		
+		return null;
 	}
 }
