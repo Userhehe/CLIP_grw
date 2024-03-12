f2ab27ecfc42c96df41bdbb63fa825b6659615e8
dongin7757 <dongin7757@gmail.com>
2024-03-13 02:24:22 +0900

    사용자, 파일업로드의 dao, impl, vo, JUnitTest, xml 추가 Config.xml파일 수정 IUserDao.java, IUserDaoImpl.java, IUserService.java, UserServiceImpl.java, IFileUploadDao.java, FileUploadDaoImpl.java, IFileUploadService.java, FileUploadServiceImpl.java, FileVo.java, userJUnitTest.java, fileUploadJUnitTest.java, User_Statement.xml, FileUpload.xml 추가

---
+++ Clip_Groupware/src/main/java/com/clip/gwr/model/service/UserServiceImpl.java
@@ -0,0 +1,131 @@
+package com.clip.gwr.model.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.clip.gwr.model.mapper.IUserDao;
+import com.clip.gwr.vo.UserVo;
+import com.clip.gwr.vo.UserinfoVo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements IUserService {
+	@Autowired
+	private IUserDao dao;
+	
+	/**
+	 * 로그인
+	 */
+	@Override
+	public UserVo userLogin(Map<String, Object> map) {
+		log.info("UserServiceImpl userLogin 로그인");
+		return dao.userLogin(map);
+	}
+
+	/**
+	 * 아이디찾기
+	 */
+	@Override
+	public UserVo findUserId(Map<String, Object> map) {
+		log.info("UserServiceImpl findUserId 아이디찾기");
+		return dao.findUserId(map);
+	}
+	
+	/**
+	 * 비밀번호 재설정
+	 */
+	@Override
+	public int updateUserPassword(Map<String, Object> map) {
+		log.info("UserServiceImpl updateUserPassword 비밀번호 재설정");
+		return dao.updateUserPassword(map);
+	}
+	
+	/**
+	 * 인증번호 저장
+	 */
+	@Override
+	public int updateUserCertnum(Map<String, Object> map) {
+		log.info("UserServiceImpl updateUserPassword 인증번호 저장");
+		return dao.updateUserCertnum(map);
+	}
+	
+	/**
+	 * 인증번호 확인
+	 */
+	@Override
+	public int comparisonCertNum(Map<String, Object> map) {
+		log.info("UserServiceImpl updateUserPassword 인증번호 확인");
+		return dao.comparisonCertNum(map);
+	}
+	
+	/**
+	 * 사원정보 등록
+	 */
+	@Override
+	public int insertUserinfo(Map<String, Object> map) {
+		log.info("UserServiceImpl insertUserinfo 사원정보등록");
+		return dao.insertUserinfo(map);
+	}
+	
+	/**
+	 * 이메일&연락처 중복체크
+	 */
+	@Override
+	public int duplicateCheckUserinfo(Map<String, Object> map) {
+		log.info("UserServiceImpl duplicateCheckUserinfo 이메일&연락처 중복체크");
+		return dao.duplicateCheckUserinfo(map);
+	}
+	
+	/**
+	 * 사원목록 전체조회
+	 */
+	@Override
+	public List<UserinfoVo> selectUserinfoList() {
+		log.info("UserServiceImpl selectUserinfoList 사원목록전체조회");
+		return dao.selectUserinfoList();
+	}
+
+	/**
+	 * 사원목록 검색
+	 */
+	@Override
+	public List<UserinfoVo> searchUserinfoList(Map<String, Object> map) {
+		log.info("UserServiceImpl searchUserinfoList 사원목록 검색");
+		return dao.searchUserinfoList(map);
+	}
+
+	
+	/**
+	 * 사원정보 상세조회
+	 */
+	@Override
+	public List<UserinfoVo> selectUserinfoDetail(Map<String, Object> map) {
+		log.info("UserServiceImpl selectUserinfoDetail 사원정보 상세조회");
+		return dao.selectUserinfoDetail(map);
+	}
+	
+	/**
+	 * 사원정보 수정
+	 */
+	@Override
+	public int updateUserinfo(Map<String, Object> map) {
+		log.info("UserServiceImpl updateUserinfo 사원정보 수정");
+		return dao.updateUserinfo(map);
+	}
+
+	
+	/**
+	 * 재직증명서 다운로드
+	 */
+	@Override
+	public List<UserinfoVo> selectJejicDownload(Map<String, Object> map) {
+		log.info("UserServiceImpl selectJejicDownload 재직증명서 다운로드");
+		return dao.selectJejicDownload(map);
+	}
+	
+}
