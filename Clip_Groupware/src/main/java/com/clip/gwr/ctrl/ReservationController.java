877586cef83010b03a97c3081b69d18093895300
pkm4949 <pkm4949@gmail.com>
2024-03-13 18:22:04 +0900

    2024.03.13
    header 및 일정관리쪽 controller 클래스 생성

--- Clip_Groupware/src/main/java/com/clip/gwr/ctrl/ReservationController.java
+++ Clip_Groupware/src/main/java/com/clip/gwr/ctrl/ReservationController.java
@@ -1,5 +1,17 @@
 package com.clip.gwr.ctrl;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
 public class ReservationController {
 
+	@GetMapping(value = "resrvation.do")
+	public String roomreservation() {
+		log.info("ReservationController roomreservation 회의실 예약화면");
+		return null;
+	}
 }
