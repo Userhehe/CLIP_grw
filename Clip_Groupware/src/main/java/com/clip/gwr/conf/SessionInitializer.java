862aebe5be16fecd2f8417935121269e5273a4d9
dongin7757 <dongin7757@gmail.com>
2024-03-26 17:42:53 +0900

    2024-03-26
    redis 비활성화

--- Clip_Groupware/src/main/java/com/clip/gwr/conf/SessionInitializer.java
+++ Clip_Groupware/src/main/java/com/clip/gwr/conf/SessionInitializer.java
@@ -1,11 +1,11 @@
-package com.clip.gwr.conf;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-/**
- * AbstractHttpSessionApplicationInitializer WebApplicationInitializer 인터페이스를 구현하고 있고 Web Application에서 HttpSession을 초기화하는데 사용 
- * extends만 해주면 Spring 세션 구성을 자동으로 설정해주고 생성자를 통해 추가적인 설정을 구성할 수 있다 
- */
-public class SessionInitializer extends AbstractHttpSessionApplicationInitializer{
-
-}
+//package com.clip.gwr.conf;
+//
+//import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+//
+///**
+// * AbstractHttpSessionApplicationInitializer WebApplicationInitializer 인터페이스를 구현하고 있고 Web Application에서 HttpSession을 초기화하는데 사용 
+// * extends만 해주면 Spring 세션 구성을 자동으로 설정해주고 생성자를 통해 추가적인 설정을 구성할 수 있다 
+// */
+//public class SessionInitializer extends AbstractHttpSessionApplicationInitializer{
+//
+//}
